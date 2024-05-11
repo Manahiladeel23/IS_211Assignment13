9ea566bd0e8943252ed319ed97a34fe913d442f1
Manahiladeel23 <158002625+Manahiladeel23@users.noreply.github.com>
2024-05-11 02:54:33 -0400

    commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# recursion.py
+
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+# recursion.py
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+# recursion.py
+
+def compareTo(s1, s2):
+    if len(s1) == 0 and len(s2) == 0:
+        return 0
+    elif len(s1) == 0:
+        return -1
+    elif len(s2) == 0:
+        return 1
+    elif s1[0] < s2[0]:
+        return -1
+    elif s1[0] > s2[0]:
+        return 1
+    else:
+        return compareTo(s1[1:], s2[1:])
